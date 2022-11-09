fb3cf7f0042bbe211cc370b63eeb15a5e58166b2
Saeed Rateb Abrar <saeedrabrar@gmail.com>
2022-11-09 03:16:53 +0000

    added Nexus tf files

---
+++ infrastructure/msp-26-nexus-server-template/nexus-vars.tf
@@ -0,0 +1,8 @@
+variable "FirstKey.pem" {}
+variable "ami" {
+  description = "amazon linux 2 ami"
+}
+variable "region" {}
+variable "instance_type" {}
+variable "nexus_server_secgr" {}
+variable "nexus-server-tag" {}
