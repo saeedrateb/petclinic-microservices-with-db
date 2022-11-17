6585af43bac13334da7ce6486ead95c5026625ce
Saeed Rateb Abrar <saeedrabrar@gmail.com>
2022-11-17 01:29:19 +0000

    added Nexu setup files

--- infrastructure/msp-26-nexus-server-template/nexus-vars.tf
+++ infrastructure/msp-26-nexus-server-template/nexus-vars.tf
@@ -1,8 +1,6 @@
-variable "FirstKey" {}
-variable "ami" {
-  description = "amazon linux 2 ami"
-}
-variable "region" {}
-variable "instance_type" {}
-variable "nexus_server_secgr" {}
-variable "nexus-server-tag" {}
+mykey = "petclinic"
+ami = "ami-0cff7528ff583bf9a"
+region = "us-east-1"
+instance_type = "t3a.medium"
+nexus_server_secgr = "petclinic-nexus-server-secgr"
+nexus-server-tag = "Nexus Server of Petclinic"
